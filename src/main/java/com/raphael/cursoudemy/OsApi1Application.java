1578612e726dc109f0dbd2e5e52c1ce03b68da52
Raphael Brangioni <raphaelbrangioni@gmail.com>
2020-05-20 22:52:16 -0300

    Projeto criado

---
+++ src/main/java/com/raphael/cursoudemy/OsApi1Application.java
@@ -0,0 +1,13 @@
+package com.raphael.cursoudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsApi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsApi1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/raphael/cursoudemy/OsApi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raphael.cursoudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsApi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
