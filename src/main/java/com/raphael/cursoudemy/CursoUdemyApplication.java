bf5f5f61ab1855d08e9c23da409e32d3be72225b
Raphael Brangioni <raphaelbrangioni@gmail.com>
2020-05-20 23:19:04 -0300

    Rest testado

--- src/main/java/com/raphael/cursoudemy/CursoUdemyApplication.java
+++ src/main/java/com/raphael/cursoudemy/CursoUdemyApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OsApi1Application {
+public class CursoUdemyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OsApi1Application.class, args);
+		SpringApplication.run(CursoUdemyApplication.class, args);
 	}
 
 }
--- src/test/java/com/raphael/cursoudemy/CursoUdemyApplicationTests.java
+++ src/test/java/com/raphael/cursoudemy/CursoUdemyApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class OsApi1ApplicationTests {
+class CursoUdemyApplicationTests {
 
 	@Test
 	void contextLoads() {
