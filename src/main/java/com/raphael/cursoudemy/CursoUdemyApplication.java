e42a0d9856adf02ba2e39c6108697129f76623a5
Raphael Brangioni <raphaelbrangioni@gmail.com>
2020-05-21 22:49:55 -0300

    Criando opeação de instanciação

--- src/main/java/com/raphael/cursoudemy/CursoUdemyApplication.java
+++ src/main/java/com/raphael/cursoudemy/CursoUdemyApplication.java
@@ -1,13 +1,32 @@
 package com.raphael.cursoudemy;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.raphael.cursoudemy.domain.Categoria;
+import com.raphael.cursoudemy.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursoUdemyApplication {
+public class CursoUdemyApplication implements CommandLineRunner{
+	
+	@Autowired
+	CategoriaRepository categoriaRespository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoUdemyApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escitório");
+		 categoriaRespository.saveAll(Arrays.asList(cat1, cat2));
+		
+		
 	}
 
 }
