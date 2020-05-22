1b16645b075fda5f59c4e95482171e8db7bac6d1
Raphael Brangioni <raphaelbrangioni@gmail.com>
2020-05-21 21:41:48 -0300

    Primaira classe de dominio - Categoria

--- src/main/java/com/raphael/cursoudemy/resources/CategoriaResource.java
+++ src/main/java/com/raphael/cursoudemy/resources/CategoriaResource.java
@@ -1,18 +1,29 @@
 package com.raphael.cursoudemy.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.raphael.cursoudemy.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
+	public List<Categoria> listar() {
 		
-		return "Rest está funcionando!";
+		Categoria cat1  = new Categoria(1, "Informáica");
+		Categoria cat2  = new Categoria(2, "Escritório");
 		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }
